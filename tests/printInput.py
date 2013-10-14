c6928a6070aaf0fadfb1d9f4071a8440224a247a
Mario Alviano <mario@alviano.net>
2013-10-14 12:21:36 +0200

    Add script to extract input from testcases.

---
+++ tests/printInput.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import sys
+
+if len(sys.argv) != 2:
+    sys.exit("Usage:\n\t%s <testcase>" % (sys.argv[0],))
+
+testcase = sys.argv[1]
+
+exec(open(testcase).read())
+for i in ['input']:
+    if i not in locals():
+        sys.exit("Testcase %s does not provide variable '%s'" % (testcase, i))
+        
+print(input)
