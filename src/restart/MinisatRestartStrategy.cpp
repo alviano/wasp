c352c31d488753886c6804b18d2d91efb5ec6be4
Mario Alviano <mario@alviano.net>
2013-10-23 22:05:50 +0200

    Change copyright in cpp.

--- src/restart/MinisatRestartStrategy.cpp
+++ src/restart/MinisatRestartStrategy.cpp
@@ -1,6 +1,6 @@
 /*
  *
- *  Copyright 2013 Mario Alviano, Carmine Dodaro, Wolfgang Faber, Nicola Leone, Francesco Ricca, and Marco Sirianni.
+ *  Copyright 2013 Mario Alviano, Carmine Dodaro, and Francesco Ricca.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
