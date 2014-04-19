1a3885b9c58041cd38c315b5c4aa307f14e19fd6
Carmine Dodaro <carminedodaro@gmail.com>
2014-04-19 09:22:40 +0200

    Added tests for weak constraints.

---
+++ tests/weakConstraints.checker.py
@@ -0,0 +1,23 @@
+def decodeModels(string):
+    models = []        
+
+    for model in string.split("\n"):
+        model = model.strip()
+        if len(model) > 5 and model[:4] == "COST":
+            models.append(model.split("COST "))
+
+    if( len(models) > 0 ):
+        return models[-1]
+    else:
+        return ""
+    
+def checker(actualOutput, actualError):
+    global output
+    expectedModels = decodeModels(output)
+    actualModels = decodeModels(actualOutput)
+    #print(expectedModels)
+    #print(actualModels)
+    if expectedModels != actualModels:
+        reportFailure(expectedModels, actualModels)
+    else:
+        reportSuccess(expectedModels, actualModels)
