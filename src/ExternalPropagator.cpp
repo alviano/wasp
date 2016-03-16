5e8af570806f3b8d4f9373aa082f43cd5b13eb96
dodaro <carminedodaro@gmail.com>
2016-03-16 18:12:31 +0100

    Added missing files.

---
+++ src/ExternalPropagator.cpp
@@ -0,0 +1,195 @@
+/*
+ *
+ *  Copyright 2013 Mario Alviano, Carmine Dodaro, and Francesco Ricca.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+#include "ExternalPropagator.h"
+#include "heuristic/interpreters/Interpreter.h"
+#include "Literal.h"
+#include "stl/Vector.h"
+#include "Solver.h"
+#include "Clause.h"
+#include "heuristic/interpreters/MyPerlInterpreter.h"
+#include "heuristic/interpreters/MyPythonInterpreter.h"
+
+ExternalPropagator::~ExternalPropagator()
+{
+    delete interpreter;
+    delete fn;
+    clearClausesToDelete();
+}
+
+ExternalPropagator::ExternalPropagator( 
+    const string& filename,
+    unsigned int interpr ) : Propagator()
+{
+    fn = new char[ filename.length() + 1 ];
+    strcpy( fn, filename.c_str() );
+    interpreter = NULL;
+    
+    if( interpr == PYTHON_INTERPRETER )
+        interpreter = new MyPythonInterpreter( fn );
+    else if( interpr == PERL_INTERPRETER )
+        interpreter = new MyPerlInterpreter( fn );
+    else
+        ErrorMessage::errorGeneric( "Unsupported interpreter" );
+    checkWellFormed();
+    check_addedVarName = interpreter->checkMethod( method_plugins_addedVarName );
+    check_onAtomElimination = interpreter->checkMethod( method_plugins_onAtomElimination );
+    check_onLitAtLevelZero = interpreter->checkMethod( method_plugins_onLitAtLevelZero );
+}
+
+bool
+ExternalPropagator::onLiteralFalse(
+    Solver& solver,
+    Literal literal,
+    int pos )
+{
+    vector< int > output;
+    interpreter->callListMethod( method_plugins_onLiteralFalse, literal.getId(), pos, output );
+    trail.push_back( literal );
+    if( output.empty() )
+        return false;
+    
+    Clause* reason = getReason( solver );    
+    //TODO: check for backjumping
+    for( unsigned int i = 0; i < output.size(); i++ )
+    {
+        Literal lit( output[ i ], true, true );
+        trail.push_back( lit );
+        solver.assignLiteral( lit, reason );
+    }
+    clausesToDelete.push_back( reason );
+    return true;
+}
+
+void
+ExternalPropagator::reset(
+    const Solver& solver )
+{
+    if( solver.getCurrentDecisionLevel() == 0 )
+        clearClausesToDelete();
+    vector< int > parameters;
+    while( !trail.empty() )
+    {
+        Literal lit = trail.back();
+        if( !solver.isUndefined( lit ) )
+            break;
+        parameters.push_back( lit.getId() );
+        trail.pop_back();
+    }
+
+    if( !parameters.empty() )
+        interpreter->callVoidMethod( method_plugins_onLiteralsUndefined, parameters );
+}
+
+void
+ExternalPropagator::attachWatches(
+    Solver& solver )
+{
+    vector< int > output;
+    interpreter->callListMethod( method_plugins_getLiterals, solver.numberOfVariables(), output );
+    for( unsigned int i = 0; i < output.size(); i++ )
+    {
+        Literal lit( output[ i ], true, true );
+        solver.setFrozen( lit.getVariable() );
+        solver.addPropagator( lit.getOppositeLiteral(), this, i );
+    }
+    output.clear();
+    interpreter->callListMethod( method_plugins_getAtomsToFreeze, output );
+    for( unsigned int i = 0; i < output.size(); i++ )
+    {
+        Literal lit( output[ i ], true, true );
+        solver.setFrozen( lit.getVariable() );
+    }
+}
+
+void
+ExternalPropagator::checkWellFormed()
+{
+    bool check = interpreter->checkMethod( method_plugins_onLiteralFalse ) &&
+                 interpreter->checkMethod( method_plugins_onLiteralsUndefined ) &&
+                 interpreter->checkMethod( method_plugins_getLiterals ) &&
+                 interpreter->checkMethod( method_plugins_getReason ) &&
+                 interpreter->checkMethod( method_plugins_getAtomsToFreeze );
+    
+    if( !check )
+    {
+        ErrorMessage::errorGeneric( "Methods " + 
+                                    string( method_plugins_onLiteralFalse ) + ", " + 
+                                    string( method_plugins_onLiteralsUndefined ) + ", " + 
+                                    string( method_plugins_getLiterals ) + ", and " + 
+                                    string( method_plugins_getReason ) + ", and " +
+                                    string( method_plugins_getAtomsToFreeze ) +
+                                    " are mandatory." );
+    }
+}
+
+Clause*
+ExternalPropagator::getReason(
+    const Solver& solver ) const
+{
+    vector< int > output;
+    interpreter->callListMethod( method_plugins_getReason, output );
+    Clause* clause = new Clause();
+    clause->addLiteral( Literal::null );
+    unsigned int max = 0;
+    for( unsigned int i = 0; i < output.size(); i++ )
+    {
+        Literal l( output[ i ], true, true );        
+        clause->addLiteral( l );
+        if( solver.getDecisionLevel( l ) > max )
+            max = solver.getDecisionLevel( l );
+    }
+    
+    if( max < solver.getCurrentDecisionLevel() )
+        ErrorMessage::errorGeneric( "Propagator cannot unroll" );
+    
+    return clause;
+}
+
+void
+ExternalPropagator::clearClausesToDelete()
+{
+    for( unsigned int i = 0; i < clausesToDelete.size(); i++ )
+        delete clausesToDelete[ i ];
+    clausesToDelete.clear();
+}
+
+void
+ExternalPropagator::addedVarName(
+    Var var,
+    const string& name )
+{
+    if( check_addedVarName )
+        interpreter->callVoidMethod( method_plugins_addedVarName, var, name );
+}
+
+void
+ExternalPropagator::onAtomElimination(
+    Var var )
+{
+    if( check_onAtomElimination )
+        interpreter->callVoidMethod( method_plugins_onAtomElimination, var );
+}
+
+void
+ExternalPropagator::onLitAtLevelZero(
+    Literal lit )
+{
+    if( check_onLitAtLevelZero )
+        interpreter->callVoidMethod( method_plugins_onLitAtLevelZero, lit.isPositive() ? lit.getVariable() : -lit.getVariable() );
+}
---
+++ src/ExternalPropagator.h
@@ -0,0 +1,68 @@
+/*
+ *
+ *  Copyright 2013 Mario Alviano, Carmine Dodaro, and Francesco Ricca.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+#ifndef EXTERNALPROPAGATOR_H
+#define EXTERNALPROPAGATOR_H
+
+#include "Propagator.h"
+#include "./stl/Vector.h"
+class Clause;
+class Interpreter;
+
+#define method_plugins_addedVarName "addedVarName"
+#define method_plugins_onAtomElimination "onAtomElimination" 
+#define method_plugins_getLiterals "getLiterals"
+#define method_plugins_getAtomsToFreeze "getAtomsToFreeze"
+#define method_onStartingSolver "onStartingSolver"
+#define method_plugins_onLiteralFalse "onLiteralFalse"
+#define method_plugins_onLiteralsUndefined "onLiteralsUndefined"
+#define method_plugins_onLitAtLevelZero "onLitAtLevelZero"
+#define method_plugins_getReason "getReason"
+
+class ExternalPropagator : public Propagator
+{
+    public:
+        ExternalPropagator( const string& filename, unsigned int interpr );
+        virtual ~ExternalPropagator();
+        
+        virtual bool onLiteralFalse( Solver& solver, Literal literal, int pos );
+        virtual void reset( const Solver& solver );
+        
+        void addedVarName( Var var, const string& name );
+        void onAtomElimination( Var var );
+        void endParsing( Solver& solver ) { attachWatches( solver ); }
+        void onLitAtLevelZero( Literal lit );
+        
+    private:
+        inline ExternalPropagator( const ExternalPropagator& orig );
+        void checkWellFormed();
+        Clause* getReason( const Solver& solver ) const;
+        void clearClausesToDelete();        
+        void attachWatches( Solver& solver );
+        
+        Vector< Literal > trail;
+        Interpreter* interpreter;
+        Vector< Clause* > clausesToDelete;
+        char* fn;
+        bool check_addedVarName;
+        bool check_onAtomElimination;
+        bool check_onLitAtLevelZero;
+};
+
+#endif
+
