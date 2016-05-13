efcdcbde5b6beef90282480706946b5ada4f85d1
dodaro <carminedodaro@gmail.com>
2016-05-13 12:06:16 +0200

    Added missing file.

---
+++ src/outputBuilders/IdOutputBuilder.h
@@ -0,0 +1,44 @@
+/*
+ *
+ *  Copyright 2013 Mario Alviano, Carmine Dodaro, and Francesco Ricca.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+#ifndef IDOUTPUTBUILDER_H
+#define IDOUTPUTBUILDER_H
+
+#include "OutputBuilder.h"
+
+class IdOutputBuilder : public OutputBuilder
+{
+    public:
+        inline IdOutputBuilder();
+        virtual void startModel();
+        virtual void printVariable( Var, bool );
+        virtual void endModel();
+        virtual void onProgramIncoherent();
+        virtual void greetings();
+        virtual void optimumFound();
+
+    private:
+        unsigned int numberOfModels;
+};
+
+IdOutputBuilder::IdOutputBuilder() : OutputBuilder(), numberOfModels( 0 )
+{
+}
+
+#endif
+
