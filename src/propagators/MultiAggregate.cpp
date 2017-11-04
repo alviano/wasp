bac138bfbc34ba0648efc606569f4ddaac1ed903
dodaro <carminedodaro@gmail.com>
2017-11-04 15:21:02 +0100

    Added file.

---
+++ src/propagators/MultiAggregate.cpp
@@ -0,0 +1,385 @@
+/*
+*
+*  Copyright 2013 Mario Alviano, Carmine Dodaro, and Francesco Ricca.
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+*
+*/
+
+#include "MultiAggregate.h"
+
+#include "../Solver.h"
+#include "../Literal.h"
+
+ostream&
+operator<<( 
+    ostream& out, 
+    const MultiAggregate& aggregate )
+{
+    return aggregate.print( out );
+}
+
+bool
+MultiAggregate::onLiteralFalse( Solver& solver, Literal lit, PropagatorData propData )
+{
+    int pos = propData.position();
+    assert( pos != 0 );
+    lit = lit.getOppositeLiteral();
+    trace_msg( multiaggregates, 1, "Literal " << lit << " in position " << pos << " is true " << ( propData.isId() ? "(id)" : "(not id)" ) );
+    return propData.isId() ? notificationIdLit( solver, lit, pos ) : notificationAggregateLit( solver, lit, pos );
+}
+
+bool MultiAggregate::notificationIdLit( Solver& solver, Literal lit, int p )
+{
+    assert( p != 0 );
+    unsigned int pos = p > 0 ? p : -p;
+    if( ( p > 0 && pos <= w1 ) || ( p < 0 && pos >= w2 ) ) return false;    
+    if( solver.getImplicant( lit.getVariable() ) != this )        
+        addInTrail( solver, lit, true, p );
+    if( p > 0 )
+    {
+        assert( pos > w1 );
+        trace_msg( multiaggregates, 2, "Updating w1 to " << pos << " (was " << w1 << ")" );
+        w1 = pos;
+        assert( solver.isTrue( ids[ w1 ] ) );
+        checkTrueInference( solver, lit );        
+    }
+    else
+    {
+        assert( pos < w2 );
+        trace_msg( multiaggregates, 2, "Updating w2 to " << pos << " (was " << w2 << ")" );
+        w2 = pos;
+        assert( solver.isFalse( ids[ w2 ] ) );        
+        checkFalseInference( solver, lit );        
+    }
+    return true;
+}
+
+bool MultiAggregate::notificationAggregateLit( Solver& solver, Literal lit, int p )
+{
+    assert( p != 0 );
+    if( solver.getImplicant( lit.getVariable() ) != this )
+        addInTrail( solver, lit, false, p );    
+    
+    if( solver.getDecisionLevel( lit ) > 0 )
+    {
+        assert( positionInTrail.find( lit.getId() ) != positionInTrail.end() );
+        assert( positionInTrail[ lit.getId() ] < trail.size() );
+        trail[ positionInTrail[ lit.getId() ] ].setPropagated();
+    }
+    
+    if( p > 0 )
+    {
+        unsigned int pos = p;        
+        trace_msg( multiaggregates, 2, "Updating current sum to " << ( currentSum + getWeight( pos ) ) << " (was " << currentSum << ")");           
+        currentSum += getWeight( pos );
+        
+        if( currentSum >= getBound( w1 + 1 ) )
+        {
+            assert( solver.isTrue( getId( w1 ) ) );            
+            unsigned int tmp = w1;
+            while( currentSum >= getBound( tmp + 1 ) )
+                ++tmp;
+            if( !inferLiteral( solver, getId( tmp ), true, tmp ) )
+                return true;
+        }
+        checkFalseInference( solver, lit );
+    }
+    else
+    {
+        unsigned int pos = -p;
+        trace_msg( multiaggregates, 2, "Updating max possible sum to " << ( maxPossibleSum - getWeight( pos ) ) << " (was " << maxPossibleSum << ")");
+        maxPossibleSum -= getWeight( pos );        
+
+        if( maxPossibleSum < getBound( w2 - 1 ) )
+        {
+            assert( solver.isFalse( getId( w2 ) ) );
+            unsigned int tmp = w2;
+            while( maxPossibleSum < getBound( tmp - 1 ) )
+                --tmp;
+            if( !inferLiteral( solver, getId( tmp ).getOppositeLiteral(), true, -tmp ) )
+                return true;            
+        }
+        checkTrueInference( solver, lit );
+    }
+    return true;
+}
+
+void
+MultiAggregate::addInTrail( Solver& solver, Literal lit, bool isId, int pos )
+{
+    assert( !solver.isUndefined( lit ) );
+    if( solver.getDecisionLevel( lit ) > 0 )
+    {
+        //assert( !isInTrail( pos, isId ) );
+        trace_msg( multiaggregates, 4, "Adding literal " << lit << " in trail (size=" << ( trail.size() + 1 ) << ") " << pos << " " << ( isId ? "(id)" : "(not id)" ) << " ");
+        positionInTrail[ lit.getId() ] = trail.size();
+        trail.push_back( TrailElement( pos, isId ) );
+    }
+}
+
+bool
+MultiAggregate::inferLiteral( Solver& solver, Literal lit, bool isId, int pos )
+{
+    if( solver.isTrue( lit ) )
+    {
+        trace_msg( multiaggregates, 3, "Trying to infer literal " << lit << " in position " << pos << " " << ( isId ? "(id)" : "(not id)" ) << " which is already true: skipped");        
+        return true;
+    }
+    trace_msg( multiaggregates, 3, "Inferring literal " << lit << " in position " << pos << " " << ( isId ? "(id)" : "(not id)" ) << " ");    
+    solver.assignLiteral( lit, this );
+    addInTrail( solver, lit, isId, pos );
+    return !solver.conflictDetected();
+}
+
+void
+MultiAggregate::checkFalseInference( Solver& solver, Literal lit )
+{
+    trace_msg( multiaggregates, 3, "Checking false inference: current sum = " << currentSum );
+    unsigned int posInTrail = solver.getPositionInTrail( lit.getVariable() );
+    for( unsigned int i = 1; i < literals.size(); i++ )
+    {
+        Literal current = getLiteral( i ).getOppositeLiteral();
+        trace_msg( multiaggregates, 4, "Considering literal " << current );        
+        if( solver.isTrue( current ) ) continue;
+        if( solver.isUndefined( current ) || solver.getPositionInTrail( current.getVariable() ) > posInTrail )
+        {
+            if( currentSum + getWeight( i ) < getBound( w2 ) ) break;
+            if( !inferLiteral( solver, current, false, -i ) )
+                return;
+        }
+    }        
+}
+
+void
+MultiAggregate::checkTrueInference( Solver& solver, Literal lit )
+{
+    trace_msg( multiaggregates, 3, "Checking true inference: max possible sum = " << maxPossibleSum );
+    unsigned int posInTrail = solver.getPositionInTrail( lit.getVariable() );
+    for( unsigned int i = 1; i < literals.size(); i++ )
+    {
+        Literal current = getLiteral( i );
+        trace_msg( multiaggregates, 4, "Considering literal " << current );
+        if( solver.isTrue( current ) ) continue;
+        if( solver.isUndefined( current ) || solver.getPositionInTrail( current.getVariable() ) > posInTrail )
+        {
+            if( maxPossibleSum - getWeight( i ) >= getBound( w1 ) ) break;
+            if( !inferLiteral( solver, current, false, i ) )
+                return;
+        }
+    }
+}
+
+void
+MultiAggregate::addBinaryClauses( Solver& solver )
+{
+    assert( solver.getCurrentDecisionLevel() == 0 );
+    //ids[ i + 1 ] -> ids[ i ]
+    for( unsigned int i = 1; i < ids.size() - 2; i++ )
+    {
+        trace_msg( multiaggregates, 1, "Adding " << ids[ i + 1 ] << "->" << ids[ i ] );
+        Clause* clause = new Clause();
+        clause->addLiteral( ids[ i + 1 ].getOppositeLiteral() );
+        clause->addLiteral( ids[ i ] );
+        solver.cleanAndAddClause( clause );
+    }
+}
+
+void
+MultiAggregate::reset( const Solver& solver )
+{
+    trace_msg( multiaggregates, 1, "Reset to level " << solver.getCurrentDecisionLevel() );
+    while( !trail.empty() )
+    {
+        TrailElement te = trail.back();
+        Literal lit = getLiteralFromTrailElement( te );
+        trace_msg( multiaggregates, 2, "Lit " << lit << " is in trail" );
+        if( !solver.isUndefined( lit ) ) break;
+        updateCounters( te );
+        trail.pop_back();        
+    }
+    while( solver.isUndefined( ids[ w1 ] ) ) --w1;
+    while( solver.isUndefined( ids[ w2 ] ) ) w2++;
+    trace_msg( multiaggregates, 3, "Restored w1=" << w1 << " and w2=" << w2 );
+    trace_msg( multiaggregates, 3, "Restored current sum=" << currentSum << " and max possible sum=" << maxPossibleSum );
+    assert_msg( solver.isTrue( ids[ w1 ] ), ids[ w1 ] << " (w1=" << w1 << "), is not true" );
+    assert_msg( solver.isFalse( ids[ w2 ] ), ids[ w2 ] << " (w2=" << w2 << "), is not false" );
+}
+
+void MultiAggregate::onLearning( const Solver& solver, Learning* strategy, Literal lit )
+{
+    vector< Literal > inferences;
+    computeInferences( inferences, lit.getOppositeLiteral() );
+    for( unsigned int i = 0; i < inferences.size(); i++ )
+        if( solver.getDecisionLevel( inferences[ i ] ) > 0 )
+            strategy->onNavigatingLiteral( inferences[ i ].getOppositeLiteral() );  
+}
+
+void MultiAggregate::onNavigatingForUnsatCore( const Solver& solver, vector<unsigned int>& visited, unsigned int numberOfCalls, Literal lit )
+{
+    vector< Literal > inferences;
+    computeInferences( inferences, lit.getOppositeLiteral() );
+    for( unsigned int i = 0; i < inferences.size(); i++ )
+        if( solver.getDecisionLevel( inferences[ i ] ) > 0 )
+            visited[ inferences[ i ].getVariable() ] = numberOfCalls;        
+}
+
+bool MultiAggregate::onNavigatingLiteralForAllMarked( const Solver&, Learning* strategy, Literal lit )
+{
+    vector< Literal > inferences;
+    computeInferences( inferences, lit.getOppositeLiteral() );    
+    for( unsigned int i = 0; i < inferences.size(); i++ )
+        if( !strategy->onNavigatingLiteralForAllMarked( inferences[ i ].getOppositeLiteral() ) )
+            return false;
+    return true;
+}
+
+void MultiAggregate::computeInferences( vector< Literal >& inferences, Literal lit )
+{
+    assert_msg( positionInTrail.find( lit.getId() ) != positionInTrail.end(), "Cannot find " << lit << " in trail" );
+    assert( inferences.empty() );
+    unsigned int position = positionInTrail[ lit.getId() ];    
+    assert_msg( position < trail.size(), position << " >= " << trail.size() );
+    trace_msg( multiaggregates, 3, "Computing inferences for literal " << lit );
+    computeInferences( inferences, position, trail[ position ].isId(), trail[ position ].position() );
+    trace_action( multiaggregates, 5, { trace_tag( cerr, multiaggregates, 5 ); cerr << "Inferences are:"; for( unsigned int i = 0; i < inferences.size(); i++ ) cerr << " " << inferences[ i ]; cerr << endl; } );
+}
+
+void MultiAggregate::computeInferences( vector< Literal >& inferences, unsigned int position, bool isId, int type )
+{
+    assert_msg( position < trail.size(), position << " >= " << trail.size() );
+    trace_msg( multiaggregates, 4, "Computing inferences: position=" << position << ", is id=" << ( isId ? "yes" : "no" ) << ", type=" << type );
+    if( isId )
+    {
+        for( unsigned int i = 0; i < position; i++ )
+        {
+            assert_msg( i < trail.size(), i << " >= " << trail.size() );
+            int pos = trail[ i ].position();
+            bool sameSign = ( pos > 0 && type > 0 ) || ( pos < 0 && type < 0 );
+            if( sameSign && !trail[ i ].isId() )
+                inferences.push_back( pos > 0 ? literals[ pos ] : literals[ -pos ].getOppositeLiteral() );
+        }
+    }
+    else
+    {
+        unsigned int lastId = UINT_MAX;        
+        for( unsigned int i = 0; i < position; i++ )
+        {
+            assert_msg( i < trail.size(), i << " >= " << trail.size() );
+            int pos = trail[ i ].position();
+            bool sameSign = ( pos > 0 && type > 0 ) || ( pos < 0 && type < 0 );
+            if( sameSign || trail[ i ].isId() )
+            {
+                if( sameSign && trail[ i ].isId() ) lastId = i;
+                continue;
+            }
+            if( abs( pos ) == abs( type ) ) continue;
+            inferences.push_back( pos > 0 ? literals[ pos ] : literals[ -pos ].getOppositeLiteral() );
+        }
+        if( lastId != UINT_MAX )
+        {
+            assert_msg( lastId < trail.size(), lastId << " >= " << trail.size() );
+            int pos = trail[ lastId ].position();
+            inferences.push_back( pos > 0 ? ids[ pos ] : ids[ -pos ].getOppositeLiteral() );        
+        }
+    }
+}
+
+ostream&
+MultiAggregate::print( ostream& out ) const
+{
+    assert( !literals.empty() );
+    assert( literals[ 0 ] == Literal::null );    
+    
+    out << "[undef=undef";
+    for( unsigned i = 1; i < literals.size(); ++i )
+        out << ", " << literals[ i ] << "=" << weights[ i ];
+    out << "]";
+    
+    assert( bounds.size() >= 2 );
+    out << " bounds: ";
+    out << "[";
+    for( unsigned int i = 0; i < bounds.size() - 1; i++ )
+        out << bounds[ i ] << ", ";
+    out << bounds.back();
+    out << "]";
+    
+    assert( ids.size() >= 2 );
+    out << " ids: ";
+    out << "[";
+    for( unsigned int i = 0; i < ids.size() - 1; i++ )
+        out << ids[ i ] << ", ";
+    out << ids.back();
+    out << "]";
+        
+    return out;
+}
+
+void
+MultiAggregate::attach( Solver& solver )
+{    
+    computeWatches( solver );
+    trace_msg( multiaggregates, 1, "Attaching aggregate " << *this );
+    for( unsigned int i = 1; i < literals.size(); i++ )
+    {
+        solver.setFrozen( literals[ i ].getVariable() );
+        solver.addPropagator( literals[ i ], this, PropagatorData( -i, false ) );
+        solver.addPropagator( literals[ i ].getOppositeLiteral(), this, PropagatorData( i, false ) );
+    }
+    
+    for( unsigned int i = 1; i < ids.size() - 1; i++ )
+    {
+        solver.setFrozen( ids[ i ].getVariable() );
+        solver.addPropagator( ids[ i ], this, PropagatorData( -i, true ) );
+        solver.addPropagator( ids[ i ].getOppositeLiteral(), this, PropagatorData( i, true ) );
+    }    
+    trace_msg( multiaggregates, 1, "Current sum " << currentSum << " - max possible sum " << maxPossibleSum );    
+}
+
+void
+MultiAggregate::computeWatches( Solver& solver )
+{
+    w2 = ids.size() - 1;
+    for( unsigned int i = 1; i < ids.size() - 1; i++ )
+    {
+        if( solver.isTrue( ids[ i ] ) )
+            w1 = i;
+        else if( solver.isFalse( ids[ i ] ) )
+        {
+            w2 = i;
+            break;
+        }
+    }
+    
+    trace_msg( multiaggregates, 1, "w1=" << w1 << " and w2=" << w2 );
+//    assert_msg( maxUnd < weights.size() && currentSum + getWeight( maxUnd ) < getBound( w2 ), "Missing inferences" );    
+//    assert_msg( maxUnd < weights.size() && maxPossibleSum - getWeight( maxUnd ) >= getBound( w1 ), "Missing inferences" );
+}
+
+#ifndef NDEBUG
+bool MultiAggregate::checkUndefined( Solver& solver )
+{
+    for( unsigned int i = 1; i < literals.size(); i++ )
+        if( !solver.isUndefined( literals[ i ] ) || solver.hasBeenEliminated( literals[ i ].getVariable() ) )
+            return false;        
+    return true;
+}
+
+bool MultiAggregate::isInTrail( int position, bool isId )
+{
+    for( unsigned int i = 0; i < trail.size(); i++ )
+        if( abs( trail[ i ].position() ) == abs( position ) && trail[ i ].isId() == isId )
+            return true;
+    return false;
+}
+#endif
---
+++ src/propagators/MultiAggregate.h
@@ -0,0 +1,185 @@
+/*
+*
+*  Copyright 2013 Mario Alviano, Carmine Dodaro, and Francesco Ricca.
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+*
+*/
+
+#ifndef WASP_MULTIAGGREGATE_H
+#define	WASP_MULTIAGGREGATE_H
+
+class Solver;
+#include "../Literal.h"
+
+#include "Propagator.h"
+#include "../Reason.h"
+#include "../util/WaspAssert.h"
+#include "../util/WaspOptions.h"
+#include "../util/WaspTrace.h"
+
+#include <vector>
+#include <unordered_map>
+#include <cstdlib>
+#include <numeric>
+using namespace std;
+
+class TrailElement
+{
+    public:
+        TrailElement( int position, bool isId ) { position_ = position > 0 ? position : -position; isPositive_ = position > 0 ? 1 : 0; isId_ = isId; propagated_ = false; }        
+        bool isId() const { return isId_; }
+        int position() const { return isPositive_ ? position_ : -position_; }
+        void setPropagated() { propagated_ = 1; }
+        bool propagated() const { return propagated_; }
+
+    private:
+        unsigned int position_ : 29;
+        unsigned int isPositive_ : 1;
+        unsigned int isId_ : 1;
+        unsigned int propagated_ : 1;
+};
+
+class MultiAggregate : public Propagator, public Reason
+{
+    friend ostream& operator<<( ostream& out, const MultiAggregate& aggregate );
+    public:        
+        inline MultiAggregate() : currentSum( 0 ), maxPossibleSum( 0 ), w1( 0 ), w2( 0 ) { addLiteral( Literal::null, 0 ); addBound( Literal( 1, NEGATIVE ), 0 ); }
+        virtual ~MultiAggregate() {}
+        bool onLiteralFalse( Solver& solver, Literal literal, PropagatorData propData );        
+        inline void addLiteral( Literal literal, uint64_t weight ) { literals.push_back( literal ); weights.push_back( weight ); maxPossibleSum += weight; }
+        inline unsigned int hasBound( uint64_t bound )
+        {
+            for( unsigned int i = 0; i < bounds.size(); i++ )
+                if( bounds[ i ] == bound )
+                    return i;
+            return UINT_MAX;
+        }
+        inline void addBound( Literal boundId, uint64_t bound ) { assert( hasBound( bound ) == UINT_MAX ); ids.push_back( boundId ); bounds.push_back( bound ); }
+                
+        void reset( const Solver& solver );
+        void simplifyAtLevelZero( Solver& ) {}
+
+        void onLearning( const Solver& solver, Learning* strategy, Literal lit );
+        bool onNavigatingLiteralForAllMarked( const Solver& solver, Learning* strategy, Literal lit );
+        void onNavigatingForUnsatCore( const Solver& solver, vector< unsigned int >& visited, unsigned int numberOfCalls, Literal lit );        
+        
+        inline void sort() { bubblesort( literals, weights, true ); bubblesort( ids, bounds, false ); }
+        void attach( Solver& solver );
+        
+        inline Literal getId( unsigned int pos ) const { assert_msg( pos < ids.size(), pos << ">=" << ids.size() ); return ids[ pos ]; }        
+        
+        inline void finalize( Solver& solver ) { sort(); addBound( Literal( 1, POSITIVE ), UINT64_MAX ); addBinaryClauses( solver ); assert( checkUndefined( solver ) ); }                        
+        
+    protected:
+        virtual ostream& print( ostream& out ) const;
+    
+    private:
+        vector< Literal > literals;
+        vector< uint64_t > weights;
+        
+        vector< Literal > ids;
+        vector< uint64_t > bounds;
+        
+        uint64_t currentSum;
+        uint64_t maxPossibleSum;
+        
+        vector< TrailElement > trail;
+        
+        unordered_map<int, unsigned int> positionInTrail; 
+        
+        unsigned int w1;
+        unsigned int w2;        
+        
+        bool notificationIdLit( Solver&, Literal, int );
+        bool notificationAggregateLit( Solver&, Literal, int );
+        
+        void addInTrail( Solver& solver, Literal lit, bool isId, int pos );                
+        bool inferLiteral( Solver& solver, Literal lit, bool isId, int pos );
+        void checkFalseInference( Solver& solver, Literal lit );
+        void checkTrueInference( Solver& solver, Literal lit );
+        
+        inline uint64_t getWeight( unsigned int pos ) const { assert_msg( pos < weights.size(), pos << ">=" << weights.size() ); return weights[ pos ]; }
+        inline uint64_t getBound( unsigned int pos ) const { assert_msg( pos < bounds.size(), pos << ">=" << bounds.size() ); return bounds[ pos ]; }
+        inline Literal getLiteral( unsigned int pos ) const { assert_msg( pos < literals.size(), pos << ">=" << literals.size() ); return literals[ pos ]; }
+        
+        void addBinaryClauses( Solver& solver );
+        
+        void computeInferences( vector< Literal >& inferences, Literal lit );
+        void computeInferences( vector< Literal >& inferences, unsigned int position, bool isId, int type );                
+                
+        inline Literal getLiteralFromTrailElement( TrailElement te )
+        {
+            int pos = te.position();
+            if( te.isId() )
+                return pos > 0 ? getId( pos ) : getId( -pos ).getOppositeLiteral();
+            else
+                return pos > 0 ? getLiteral( pos ) : getLiteral( -pos ).getOppositeLiteral();
+        }
+        
+        inline void updateCounters( TrailElement te )
+        {
+            trace_msg( multiaggregates, 2, "Updating counters. Considering element in position " << te.position() << " which is " << ( te.isId() ? "" : "not" ) << " id" );
+            if( te.isId() || !te.propagated() )
+                return;
+            int pos = te.position();
+            unsigned int p;
+            if( pos > 0 )
+            {
+                p = pos;
+                assert_msg( currentSum >= getWeight( p ), "Error while restoring current sum: " << currentSum << " < " << getWeight( p ) );
+                currentSum -= getWeight( p );                
+            }
+            else
+            {   
+                p = -pos;                
+                assert_msg( maxPossibleSum + getWeight( p ) <= ( unsigned int ) accumulate( weights.begin(), weights.end(), 0 ), "Error while restoring max possible sum: " << maxPossibleSum << " + " << getWeight( p ) << " > " << accumulate( weights.begin(), weights.end(), 0 ) );
+                maxPossibleSum += getWeight( p );
+            }        
+        }
+        
+        template<class T>
+        void swap( vector< T >& elems, unsigned int j )
+        {
+            T tmp = elems[ j ]; 
+            elems[ j ] = elems[ j + 1 ]; 
+            elems[ j + 1 ] = tmp;
+        }
+        
+        void bubblesort( vector< Literal >& lits, vector< uint64_t >& ws, bool descOrd )
+        {
+            assert( lits.size() == ws.size() );            
+            for( int i = lits.size() - 1; i > 1; i-- )
+                for( int j = 1; j < i; j++ )
+                {
+                    if( descOrd )
+                    {
+                        if( ws[ j ] < ws[ j + 1 ] ) { swap( lits, j ); swap( ws, j ); } 
+                    }
+                    else
+                    {
+                        if( ws[ j ] > ws[ j + 1 ] ) { swap( lits, j ); swap( ws, j ); } 
+                    }
+                }            
+        }        
+        
+        void computeWatches( Solver& solver );
+        
+        #ifndef NDEBUG
+        bool checkUndefined( Solver& solver );
+        bool isInTrail( int position, bool isId );
+        #endif
+};
+
+#endif
+
