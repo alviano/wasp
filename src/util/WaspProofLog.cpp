baaeca98fc0563f47398759e711ca0d850f80746
Mario Alviano <mario@alviano.net>
2019-04-24 19:11:01 +0200

    missing files

---
+++ src/util/WaspProofLog.cpp
@@ -0,0 +1,81 @@
+#include "WaspProofLog.h"
+
+#include <cassert>
+
+namespace proof {
+
+#ifndef PROOF_LOG
+
+bool setLogFile( const char* ) {}
+void bodyDefinition( int ) {}
+void addition( const std::vector<int>& ) {}
+void completionRuleAddition() {}
+void completionSupportAddition() {}
+void extension( int, const std::vector<int>& ) {}
+void deletion( const std::vector<int>& ) {}
+void loop() {}
+void unfoundedSetAddition() {}
+
+#else
+
+#define log( msg, ... ) proof_log( msg, ##__VA_ARGS__ )
+
+static FILE* log_file = stderr;
+
+bool setLogFile( const char* filename ) {
+    return true;
+}
+
+void bodyDefinition( int id ) {
+    log( "b %d", id );
+    log( " 0\n" );
+}
+
+void addition( const std::vector<int>& lits ) {
+    log( "a" );
+    for(auto lit : lits) log( " %d", -lit );
+    log( " 0\n" );
+}
+
+void completionRuleAddition() {
+
+}
+
+void completionSupportAddition() {
+
+}
+
+void extension( int var, const std::vector<int>& lits ) {
+    assert( var > 0 );
+    log( "e %d", var );
+    for(auto lit : lits) log( " %d", -lit );
+    log( " 0\n" );
+}
+
+void deletion( const std::vector<int>& lits ) {
+    log( "d" );
+    for(auto lit : lits) log( " %d", -lit );
+    log( "0\n" );
+}
+
+void loop() {
+
+}
+
+void unfoundedSetAddition() {
+
+}
+
+#endif
+
+}
+
+#ifdef PROOF_LOG
+
+FILE*
+getLogFile()
+{
+    return proof::log_file;
+}
+
+#endif
---
+++ src/util/WaspProofLog.h
@@ -0,0 +1,51 @@
+/*
+ *
+ *  Copyright 2013 Mario Alviano, Carmine Dodaro, and Francesco Ricca.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+#ifndef WASP_PROOF_LOG_H
+#define WASP_PROOF_LOG_H
+
+#include <cstdio>
+#include <vector>
+
+#ifndef PROOF_LOG
+    #define proof_log( msg, ... )
+    #define proof_block( blck )
+#else
+    FILE* getLogFile();
+    #define proof_log( msg, ... ) \
+        fprintf( getLogFile(), msg, ##__VA_ARGS__ );
+    #define proof_block( block ) \
+        { block }
+#endif
+
+
+namespace proof {
+
+bool setLogFile( const char* filename );
+void bodyDefinition( int id );
+void addition( const std::vector<int>& lits );
+void completionRuleAddition();
+void completionSupportAddition();
+void extension( int var, const std::vector<int>& lits );
+void deletion( const std::vector<int>& lits );
+void loop();
+void unfoundedSetAddition();
+
+}
+
+#endif
